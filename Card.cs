5e3a5311426a494e958f6bffa7794b90b7a37768
Atelier Ferrofell <kai@ferrofell.com>
2025-08-05 02:56:47 -0700

    Turn all classes into tools for editor support

--- Card.cs
+++ Card.cs
@@ -1,9 +1,20 @@
 using Godot;
 using System;
 
+[Tool]
 [GlobalClass]
 public partial class Card : Resource
 {
+	private CardInfo _info;
+	
 	[Export]
-	public CardInfo Info { get; set; }
+	public CardInfo Info
+	{
+		get => _info;
+		set
+		{
+			_info = value;
+			EmitChanged();
+		}
+	}
 }
