62526602331afc910bced621f08fcbe9eccf1fd4
Atelier Ferrofell <kai@ferrofell.com>
2025-08-05 18:23:35 -0700

    Turn Card into a node and remove boilerplate

--- Card.cs
+++ Card.cs
@@ -3,18 +3,27 @@ using System;
 
 [Tool]
 [GlobalClass]
-public partial class Card : Resource
+public partial class Card : Node
 {
 	private CardInfo _info;
-	
+
 	[Export]
 	public CardInfo Info
 	{
 		get => _info;
 		set
 		{
+			if (_info != null)
+			{
+				_info.Changed -= OnInfoChanged;
+			}
 			_info = value;
-			EmitChanged();
+			if (_info != null)
+			{
+				_info.Changed += OnInfoChanged;
+			}
 		}
 	}
+
+	protected virtual void OnInfoChanged() { }
 }
