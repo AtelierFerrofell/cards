db8ebab3d8bf1a65557c5d3aef7180a2b037c995
Atelier Ferrofell <kai@ferrofell.com>
2025-09-26 23:06:51 -0700

    Integrate draw function into Deck

--- Deck.cs
+++ Deck.cs
@@ -5,4 +5,11 @@ using System;
 [GlobalClass]
 public partial class Deck : Node
 {
+	public Card Draw(Node target)
+	{
+		Card card = GetChild(0) as Card;
+		// Handle when card is null
+		card.Reparent(target);
+		return card;
+	}
 }
--- Hand.cs
+++ Hand.cs
@@ -10,17 +10,15 @@ public partial class Hand : Node
 
 	public void Draw()
 	{
-		Card card = Deck.GetChild(0) as Card;
-		card.Reparent(this);
-		card.Reveal();
+		Deck.Draw(this).Reveal();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (GetChildCount() == 0)
-		{
-			Draw();
-		}
+		//if (GetChildCount() == 0)
+		//{
+			//Draw();
+		//}
 	}
 }
